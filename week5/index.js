c243344c2b0753148201538752d69e79efaca27a
Akshansh Lohan <akshanshlohan@gmail.com>
2025-01-08 23:11:58 +0530

    changes

--- week5/index.js
+++ week5/index.js
@@ -1,12 +1,39 @@
 const express=require('express')
 const app=express;
 
-app.length("/add/:a/:b",function(req,res){
+app.get("/add/:a/:b",function(req,res){
     const a=parseInt(req.params.a);
     const b=parseInt(req.params.b);
 
     res.json({
         sum:a+b
+    })
+
+})
+app.get("/sub/:a/:b",function(req,res){
+    const a=parseInt(req.params.a);
+    const b=parseInt(req.params.b);
+
+    res.json({
+        sub:a-b
+    })
+
+})
+app.get("/div/:a/:b",function(req,res){
+    const a=parseInt(req.params.a);
+    const b=parseInt(req.params.b);
+
+    res.json({
+        div:a/b
+    })
+
+})
+app.get("/add/:a/:b",function(req,res){
+    const a=parseInt(req.params.a);
+    const b=parseInt(req.params.b);
+
+    res.json({
+        mul:a*b
     })
 
 })
